d4a7466b2d215df804b8aab15ac4312880a2b96e
Kyle Aleshire <kjaleshire@gmail.com>
2016-02-12 00:08:59 -0600

    Initial Commit

---
+++ RustHelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RustHelloWorld
+//
+//  Created by Kyle J Aleshire on 2/12/16.
+//  Copyright © 2016 Kyle J Aleshire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
